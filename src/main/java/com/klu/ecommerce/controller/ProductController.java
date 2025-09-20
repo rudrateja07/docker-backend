dbf5d16ee45b2fedab6c69a8692d2892c9c433ec
rudrateja07 <rudrateja.chandu07@gmail.com>
2025-09-20 09:56:26 +0530

    Update ProductController.java

--- src/main/java/com/klu/ecommerce/controller/ProductController.java
+++ src/main/java/com/klu/ecommerce/controller/ProductController.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origin="*")
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     private final ProductRepository productRepository;
